1f3eb07db5b61bb787c3e3bd2170f3c97718bd98
Miłosz Dyka <milosz.dyka@gmail.com>
2025-11-28 18:11:15 +0100

    sakura 8

---
+++ src/japanese/vocabulary/sakura/sakura_8.ts
@@ -0,0 +1,107 @@
+
+import type { JapaneseWord } from "../../types";
+
+const sakura_8: JapaneseWord[] = [
+    {
+        en: "4 things",
+        pl: "Cztery rzeczy",
+        jp: "よっつ",
+    },
+    {
+        en: "Stamp",
+        pl: "Znaczek",
+        jp: "きって",
+    },
+    {
+        en: "Magazine",
+        pl: "Czasopismo",
+        jp: "ざっし",
+    },
+    {
+        en: "Eight thousand",
+        pl: "Osiem tysięcy",
+        jp: "はっせん",
+    },
+    {
+        en: "3 things",
+        pl: "Trzy rzeczy",
+        jp: "みっつ",
+    },
+    {
+        en: "One long object",
+        pl: "Jeden długi przedmiot",
+        jp: "いっぽん",
+    },
+    {
+        en: "Ticket",
+        pl: "Bilet",
+        jp: "きっぷ",
+    },
+    {
+        en: "To like",
+        pl: "Lubić",
+        jp: "すきです",
+    },
+    {
+        en: "Don't like",
+        pl: "Nie lubić",
+        jp: "すきじゃないです",
+    },
+    {
+        en: "Wine",
+        pl: "Wino",
+        jp: "ワイン",
+    },
+    {
+        en: "Beer",
+        pl: "Piwo",
+        jp: "ビール",
+    },
+    {
+        en: "Soup",
+        pl: "Zupa",
+        jp: "スープ",
+    },
+    {
+        en: "Ice cream",
+        pl: "Lody",
+        jp: "アイスクリーム",
+    },
+    {
+        en: "Cake",
+        pl: "Ciasto",
+        jp: "ケーキ",
+    },
+    {
+        en: "Sauce",
+        pl: "Sos",
+        jp: "ソース",
+    },
+    {
+        en: "Potato chips",
+        pl: "Chipsy",
+        jp: "チップス",
+    },
+    {
+        en: "Sushi",
+        pl: "Sushi",
+        jp: "すし",
+    },
+    {
+        en: "Dumplings",
+        pl: "Pierogi",
+        jp: "ピエロギ",
+    },
+    {
+        en: "No, thank you",
+        pl: "Nie, dziękuję",
+        jp: "いいえ、けっこうです",
+    },
+    {
+        en: "Restaurant",
+        pl: "Restauracja",
+        jp: "レストラン",
+    },
+];
+
+export { sakura_8 };
