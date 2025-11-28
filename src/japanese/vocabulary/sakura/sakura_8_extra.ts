58ccfcf4edfd689f921b44f8365df9d54344345b
Miłosz Dyka <milosz.dyka@gmail.com>
2025-11-28 19:25:58 +0100

    sakura 8 extras

---
+++ src/japanese/vocabulary/sakura/sakura_8_extra.ts
@@ -0,0 +1,542 @@
+
+import type { JapaneseWord } from "../../types";
+
+const sakura_8_extra: JapaneseWord[] = [
+    {
+        en: "Strawberry",
+        pl: "Truskawka",
+        jp: "いちご",
+    },
+    {
+        en: "Apple",
+        pl: "Jabłko",
+        jp: "りんご",
+    },
+    {
+        en: "Orange",
+        pl: "Pomarańcza",
+        jp: "オレンジ",
+    },
+    {
+        en: "Peach",
+        pl: "Brzoskwinia",
+        jp: "もも",
+    },
+    {
+        en: "Grape",
+        pl: "Winogrono",
+        jp: "ぶどう",
+    },
+    {
+        en: "Watermelon",
+        pl: "Arbuz",
+        jp: "すいか",
+    },
+    {
+        en: "Kaki fruit",
+        pl: "Owoc kaki",
+        jp: "かき",
+    },
+    {
+        en: "Pear",
+        pl: "Gruszka",
+        jp: "なし",
+    },
+    {
+        en: "Cherry",
+        pl: "Wiśnia",
+        jp: "さくらんぼ",
+    },
+    {
+        en: "Plum",
+        pl: "Śliwka",
+        jp: "すもも",
+    },
+    {
+        en: "Lemon",
+        pl: "Cytryna",
+        jp: "レモン",
+    },
+    {
+        en: "Banana",
+        pl: "Banan",
+        jp: "バナナ",
+    },
+    {
+        en: "Kiwi",
+        pl: "Kiwi",
+        jp: "キウイ",
+    },
+    {
+        en: "Pineapple",
+        pl: "Ananas",
+        jp: "パイナップル",
+    },
+    {
+        en: "Melon",
+        pl: "Melon",
+        jp: "メロン",
+    },
+    {
+        en: "Blueberry",
+        pl: "Jagoda",
+        jp: "ブルーベリー",
+    },
+    {
+        en: "Raspberry",
+        pl: "Malina",
+        jp: "ラズベリー",
+    },
+    {
+        en: "Mango",
+        pl: "Mango",
+        jp: "マンゴー",
+    },
+    {
+        en: "White radish",
+        pl: "Biała rzodkiew",
+        jp: "だいこん",
+    },
+    {
+        en: "Radish",
+        pl: "Rzodkiewka",
+        jp: "ラディッシュ",
+    },
+    {
+        en: "Edamame",
+        pl: "Edamame",
+        jp: "えだまめ",
+    },
+    {
+        en: "Cucumber",
+        pl: "Ogórek",
+        jp: "きゅうり",
+    },
+    {
+        en: "Leek",
+        pl: "Por",
+        jp: "ねぎ",
+    },
+    {
+        en: "Onion",
+        pl: "Cebula",
+        jp: "たまねぎ",
+    },
+    {
+        en: "Lettuce",
+        pl: "Sałata",
+        jp: "レタス",
+    },
+    {
+        en: "Cabbage",
+        pl: "Kapusta",
+        jp: "キャベツ",
+    },
+    {
+        en: "Chinese cabbage",
+        pl: "Kapusta pekińska",
+        jp: "はくさい",
+    },
+    {
+        en: "Pumpkin",
+        pl: "Dynia",
+        jp: "かぼちゃ",
+    },
+    {
+        en: "Mushroom",
+        pl: "Grzyb",
+        jp: "きのこ",
+    },
+    {
+        en: "Carrot",
+        pl: "Marchewka",
+        jp: "にんじん",
+    },
+    {
+        en: "Paprika",
+        pl: "Papryka",
+        jp: "パプリカ",
+    },
+    {
+        en: "Potato",
+        pl: "Ziemniak",
+        jp: "じゃがいも",
+    },
+    {
+        en: "Sweet potato",
+        pl: "Słodki ziemniak",
+        jp: "さつまいも",
+    },
+    {
+        en: "Beetroot",
+        pl: "Burak",
+        jp: "ビーツ",
+    },
+    {
+        en: "Broccoli",
+        pl: "Brokuł",
+        jp: "ブロッコリー",
+    },
+    {
+        en: "Eggplant",
+        pl: "Bakłażan",
+        jp: "なす",
+    },
+    {
+        en: "Corn",
+        pl: "Kukurydza",
+        jp: "とうもろこし",
+    },
+    {
+        en: "Garlic",
+        pl: "Czosnek",
+        jp: "にんにく",
+    },
+    {
+        en: "Cucinni",
+        pl: "Cukinia",
+        jp: "ズッキーニ",
+    },
+    {
+        en: "Spinach",
+        pl: "Szpinak",
+        jp: "ほうれんそう",
+    },
+    {
+        en: "Tomato",
+        pl: "Pomidor",
+        jp: "トマト",
+    },
+    {
+        en: "Asparagus",
+        pl: "Szparagi",
+        jp: "アスパラガス",
+    },
+    {
+        en: "Dog",
+        pl: "pies",
+        jp: "いぬ",
+    },
+    {
+        en: "Cat",
+        pl: "kot",
+        jp: "ねこ",
+    },
+    {
+        en: "Lion",
+        pl: "lew",
+        jp: "ライオン",
+    },
+    {
+        en: "Elephant",
+        pl: "słoń",
+        jp: "ぞう",
+    },
+    {
+        en: "Horse",
+        pl: "koń",
+        jp: "うま",
+    },
+    {
+        en: "Cow",
+        pl: "krowa",
+        jp: "うし",
+    },
+    {
+        en: "Monkey",
+        pl: "małpa",
+        jp: "さる",
+    },
+    {
+        en: "Rabbit",
+        pl: "królik",
+        jp: "うさぎ",
+    },
+    {
+        en: "Giraffe",
+        pl: "żyrafa",
+        jp: "きりん",
+    },
+    {
+        en: "Bear",
+        pl: "niedźwiedź",
+        jp: "くま",
+    },
+    {
+        en: "Fox",
+        pl: "lis",
+        jp: "きつね",
+    },
+    {
+        en: "Squirrel",
+        pl: "wiewiórka",
+        jp: "りす",
+    },
+    {
+        en: "Tiger",
+        pl: "tygrys",
+        jp: "とら",
+    },
+    {
+        en: "Gorilla",
+        pl: "goryl",
+        jp: "ゴリラ",
+    },
+    {
+        en: "Kangaroo",
+        pl: "kangur",
+        jp: "カンガルー",
+    },
+    {
+        en: "Hippopotamus",
+        pl: "hipopotam",
+        jp: "かば",
+    },
+    {
+        en: "Zebra",
+        pl: "zebra",
+        jp: "しまうま",
+    },
+    {
+        en: "Deer",
+        pl: "jeleń",
+        jp: "しか",
+    },
+    {
+        en: "Panda",
+        pl: "panda",
+        jp: "パンダ",
+    },
+    {
+        en: "Pig",
+        pl: "świnia",
+        jp: "ぶた",
+    },
+    {
+        en: "Goat",
+        pl: "koza",
+        jp: "やぎ",
+    },
+    {
+        en: "Sheep",
+        pl: "owca",
+        jp: "ひつじ",
+    },
+    {
+        en: "Alpaca",
+        pl: "alpaka",
+        jp: "アルパカ",
+    },
+    {
+        en: "Raccoon",
+        pl: "szop pracz",
+        jp: "あらいぐま",
+    },
+    {
+        en: "Raccoon Dog (Tanuki)",
+        pl: "jenot",
+        jp: "たぬき",
+    },
+    {
+        en: "Koala",
+        pl: "koala",
+        jp: "コアラ",
+    },
+    {
+        en: "Rhinoceros",
+        pl: "nosorożec",
+        jp: "さい",
+    },
+    {
+        en: "Wild Boar",
+        pl: "dzik",
+        jp: "いのしし",
+    },
+    {
+        en: "Donkey",
+        pl: "osioł",
+        jp: "ろば",
+    },
+    {
+        en: "Camel",
+        pl: "wielbłąd",
+        jp: "らくだ",
+    },
+    {
+        en: "Wolf",
+        pl: "wilk",
+        jp: "おおかみ",
+    },
+    {
+        en: "Sloth",
+        pl: "Leniwiec",
+        jp: "なまけもの",
+    },
+    {
+        en: "Chicken",
+        pl: "Kurczak",
+        jp: "にわとり",
+    },
+    {
+        en: "Hamster",
+        pl: "Chomik",
+        jp: "ハムスター",
+    },
+    {
+        en: "Mouse",
+        pl: "Mysz",
+        jp: "ねずみ",
+    },
+    {
+        en: "Hedgehog",
+        pl: "Jeż",
+        jp: "はりねずみ",
+    },
+    {
+        en: "Guinea pig",
+        pl: "Świnka morska",
+        jp: "モルモット",
+    },
+    {
+        en: "Mole",
+        pl: "Kret",
+        jp: "もぐら",
+    },
+    {
+        en: "Pigeon",
+        pl: "Gołąb",
+        jp: "はと",
+    },
+    {
+        en: "Snake",
+        pl: "Wąż",
+        jp: "へび",
+    },
+    {
+        en: "Basketball",
+        pl: "Koszykówka",
+        jp: "バスケットボール",
+    },
+    {
+        en: "Baseball",
+        pl: "Baseball",
+        jp: "やきゅう",
+    },
+    {
+        en: "Soccer",
+        pl: "Piłka nożna",
+        jp: "サッカー",
+    },
+    {
+        en: "Volleyball",
+        pl: "Siatkówka",
+        jp: "バレーボール",
+    },
+    {
+        en: "Golf",
+        pl: "Golf",
+        jp: "ゴルフ",
+    },
+    {
+        en: "Skiing",
+        pl: "Narciarstwo",
+        jp: "スキー",
+    },
+    {
+        en: "Figure Skating",
+        pl: "Łyżwiarstwo figurowe",
+        jp: "フィギュアスケート",
+    },
+    {
+        en: "Snowboarding",
+        pl: "Snowboarding",
+        jp: "スノーボード",
+    },
+    {
+        en: "Ice Hockey",
+        pl: "Hokej na lodzie",
+        jp: "アイスホッケー",
+    },
+    {
+        en: "Skating (Ice)",
+        pl: "Łyżwiarstwo",
+        jp: "スケート",
+    },
+    {
+        en: "Running",
+        pl: "Bieganie",
+        jp: "ランニング",
+    },
+    {
+        en: "Badminton",
+        pl: "Badminton",
+        jp: "バドミントン",
+    },
+    {
+        en: "Tennis",
+        pl: "Tenis",
+        jp: "テニス",
+    },
+    {
+        en: "Table Tennis",
+        pl: "Tenis stołowy",
+        jp: "たっきゅう",
+    },
+    {
+        en: "Swimming",
+        pl: "Pływanie",
+        jp: "すいえい",
+    },
+    {
+        en: "Judo",
+        pl: "Judo",
+        jp: "じゅうどう",
+    },
+    {
+        en: "Kyudo (Archery)",
+        pl: "Kyudo (Łucznictwo)",
+        jp: "きゅうどう",
+    },
+    {
+        en: "Kendo",
+        pl: "Kendo",
+        jp: "けんどう",
+    },
+    {
+        en: "Karate",
+        pl: "Karate",
+        jp: "からて",
+    },
+    {
+        en: "Boxing",
+        pl: "Boks",
+        jp: "ボクシング",
+    },
+    {
+        en: "Canoeing",
+        pl: "Kajakarstwo",
+        jp: "カヌー",
+    },
+    {
+        en: "Mountain Biking",
+        pl: "Kolarstwo górskie",
+        jp: "マウンテンバイク",
+    },
+    {
+        en: "Gymnastics",
+        pl: "Gimnastyka",
+        jp: "たいそう",
+    },
+    {
+        en: "Skateboarding",
+        pl: "Jazda na deskorolce",
+        jp: "スケートボード",
+    },
+    {
+        en: "Horseback Riding",
+        pl: "Jeździectwo",
+        jp: "じょうば",
+    },
+];
+
+export { sakura_8_extra };
