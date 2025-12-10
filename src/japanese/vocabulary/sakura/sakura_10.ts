654e7e63e41a23cc1caec8c320a0771e1a56feb1
Miłosz Dyka <milosz.dyka@gmail.com>
2025-12-10 19:38:26 +0100

    sakura 10 words

---
+++ src/japanese/vocabulary/sakura/sakura_10.ts
@@ -0,0 +1,134 @@
+import type { JapaneseWord } from "../../types";
+
+export const sakura_10: JapaneseWord[] = [
+    {
+        en: "Breakfast",
+        pl: "Śniadanie",
+        jp: "あさごはん",
+    },
+    {
+        en: "Lunch",
+        pl: "Obiad",
+        jp: "ひるごはん",
+    },
+    {
+        en: "Dinner",
+        pl: "Kolacja",
+        jp: "ばんごはん",
+    },
+    {
+        en: "Picture / Drawing",
+        pl: "Obrazek / Rysunek",
+        jp: "え",
+    },
+    {
+        en: "Music",
+        pl: "Muzyka",
+        jp: "おんがく",
+    },
+    {
+        en: "Listen",
+        pl: "Słuchać",
+        jp: "ききます",
+    },
+    {
+        en: "Radio",
+        pl: "Radio",
+        jp: "ラジオ",
+    },
+    {
+        en: "News",
+        pl: "Wiadomości",
+        jp: "ニュース",
+    },
+    {
+        en: "Hiragana",
+        pl: "Hiragana",
+        jp: "ひらがな",
+    },
+    {
+        en: "Katakana",
+        pl: "Katakana",
+        jp: "カタカナ",
+    },
+    {
+        en: "Kanji",
+        pl: "Kanji",
+        jp: "かんじ",
+    },
+    {
+        en: "Draw",
+        pl: "Rysować",
+        jp: "かきます",
+    },
+    {
+        en: "Write",
+        pl: "Pisać",
+        jp: "かきます",
+    },
+    {
+        en: "Not listen",
+        pl: "Nie słuchać",
+        jp: "ききません",
+    },
+    {
+        en: "Not write",
+        pl: "Nie pisać",
+        jp: "かきません",
+    },
+    {
+        en: "Buy",
+        pl: "Kupować",
+        jp: "かいます",
+    },
+    {
+        en: "Not buy",
+        pl: "Nie kupować",
+        jp: "かいません",
+    },
+    {
+        en: "Smartphone",
+        pl: "Smartfon",
+        jp: "スマホ",
+    },
+    {
+        en: "Do / Play",
+        pl: "Robić / Grać",
+        jp: "します",
+    },
+    {
+        en: "Not do / play",
+        pl: "Nie robić / grać",
+        jp: "しません",
+    },
+    {
+        en: "Learn",
+        pl: "Uczyć się",
+        jp: "べんきょうをします",
+    },
+    {
+        en: "Do judo",
+        pl: "Uprawiać judo",
+        jp: "じゅうどうをします",
+    },
+    {
+        en: "Play games",
+        pl: "Grać w gry",
+        jp: "ゲームをします",
+    },
+    {
+        en: "Do shopping",
+        pl: "Robić zakupy",
+        jp: "かいものをします",
+    },
+    {
+        en: "Do home work",
+        pl: "Robić pracę domową",
+        jp: "しゅくだいをします",
+    },
+    {
+        en: "Mail",
+        pl: "Mail",
+        jp: "メール",
+    },
+];
