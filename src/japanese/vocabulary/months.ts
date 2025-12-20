ea423796c207e24cc7eee0b2fb05d4017c199d0e
Miłosz Dyka <milosz.dyka@gmail.com>
2025-12-20 08:14:07 +0100

    months

---
+++ src/japanese/vocabulary/months.ts
@@ -0,0 +1,76 @@
+import type { JapaneseWord } from '../types.ts';
+
+export const months: JapaneseWord[] = [
+    {
+        en: 'January',
+        pl: 'Styczeń',
+        jp: '一月',
+        jp_pronunciation: 'いちがつ',
+    },
+    {
+        en: 'February',
+        pl: 'Luty',
+        jp: '二月',
+        jp_pronunciation: 'にがつ',
+    },
+    {
+        en: 'March',
+        pl: 'Marzec',
+        jp: '三月',
+        jp_pronunciation: 'さんがつ',
+    },
+    {
+        en: 'April',
+        pl: 'Kwiecień',
+        jp: '四月',
+        jp_pronunciation: 'しがつ',
+    },
+    {
+        en: 'May',
+        pl: 'Maj',
+        jp: '五月',
+        jp_pronunciation: 'ごがつ',
+    },
+    {
+        en: 'June',
+        pl: 'Czerwiec',
+        jp: '六月',
+        jp_pronunciation: 'ろくがつ',
+    },
+    {
+        en: 'July',
+        pl: 'Lipiec',
+        jp: '七月',
+        jp_pronunciation: 'しちがつ',
+    },
+    {
+        en: 'August',
+        pl: 'Sierpień',
+        jp: '八月',
+        jp_pronunciation: 'はちがつ',
+    },
+    {
+        en: 'September',
+        pl: 'Wrzesień',
+        jp: '九月',
+        jp_pronunciation: 'くがつ',
+    },
+    {
+        en: 'October',
+        pl: 'Październik',
+        jp: '十月',
+        jp_pronunciation: 'じゅうがつ',
+    },
+    {
+        en: 'November',
+        pl: 'Listopad',
+        jp: '十一月',
+        jp_pronunciation: 'じゅういちがつ',
+    },
+    {
+        en: 'December',
+        pl: 'Grudzień',
+        jp: '十二月',
+        jp_pronunciation: 'じゅうにがつ',
+    },
+];
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -32,6 +32,7 @@ import {
     countingThingsBag,
     genki1MajorsBag,
     ordinalNumbersBag,
+    monthsBag,
 } from '../japanese';
 import { type TranslationLanguage, TranslationLanguages } from '../TranslationLanguage';
 import { LanguageSelector } from '../components/LanguageSelector';
@@ -55,7 +56,7 @@ const MainPage: React.FC = () => {
     };
 
     const groupedBags: Record<string, WordBag[]> = {
-        'Essentials 📌': [familyBag, numbersBag, countingThingsBag, ordinalNumbersBag, daysOfWeekBag, timeBag],
+        'Essentials 📌': [familyBag, numbersBag, countingThingsBag, ordinalNumbersBag, daysOfWeekBag, timeBag, monthsBag,],
         'Genki books 📚': [genki0Bag, genki1Bag, genki1MajorsBag, genki2Bag],
         'Classes 🎓': [
             sakura1Bag,
