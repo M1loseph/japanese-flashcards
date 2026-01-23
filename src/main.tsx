f857a6384caa8c16d95f0743cdf932161823be04
Miłosz Dyka <milosz.dyka@gmail.com>
2026-01-23 11:08:52 +0100

    fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
             {
                 path: '*',
                 element: <Navigate to="/" replace />,
-            }
+            },
         ],
     },
 ]);
