b407b6dadfd215ab10e09fba40dc273510611499
Miłosz Dyka <milosz.dyka@gmail.com>
2026-02-01 11:28:25 +0100

    initial words

---
+++ src/japanese/vocabulary/sakura/trimester_2/sakura_3.ts
@@ -0,0 +1,56 @@
+import type { JapaneseWord } from '../../../types';
+
+export const sakura_2_3: JapaneseWord[] = [
+    {
+        type: 'noun',
+        en: 'Reading (process)',
+        pl: 'Czytanie',
+        jp: 'どくしょ',
+    },
+    {
+        type: 'noun',
+        en: 'Distance',
+        pl: 'Odległość',
+        jp: 'きょり',
+    },
+    {
+        type: 'verb',
+        verb_type: 'godan',
+        en: 'Chill / Calm down / Refrigerate',
+        pl: 'Ochłonąć / Schłodzić',
+        jp: 'ひやす',
+        masu_form: 'ひやします',
+        masen_form: 'ひやしません',
+    },
+    {
+        type: 'adjective',
+        adjective_type: 'i-adjective',
+        en: 'Brown',
+        pl: 'Brązowy',
+        jp: 'ちゃいろい',
+    },
+    {
+        type: 'noun',
+        en: 'Prisoner',
+        pl: 'Więzień',
+        jp: 'しゅうじん',
+    },
+    {
+        type: 'noun',
+        en: 'Landlord / Landlady',
+        pl: 'Właściciel / Właścicielka',
+        jp: 'おおや',
+    },
+    {
+        type: 'noun',
+        en: 'Century',
+        pl: 'Wiek / Stulecie',
+        jp: 'せいき',
+    },
+    {
+        type: 'noun',
+        en: 'Seat',
+        pl: 'Miejsce (siedzące)',
+        jp: 'せき',
+    },
+];
