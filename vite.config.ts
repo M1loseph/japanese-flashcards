54f162d5e589738c9af29d23412907afaad533c4
Miłosz Dyka <milosz.dyka@gmail.com>
2026-03-25 20:46:48 +0100

    fix formatting

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
                         options: {
                             networkTimeoutSeconds: 5,
                         },
-                    }
+                    },
                 ],
             },
             manifest: {
