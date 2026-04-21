e13f7dba7e96957a97733a6ba57abb31dcc137e0
Miłosz Dyka <milosz.dyka@gmail.com>
2026-04-21 19:08:39 +0200

    use eslint import plugin x

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
+import { importX } from 'eslint-plugin-import-x';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -20,19 +20,15 @@ export default defineConfig([
             tseslint.configs.strict,
             reactRefresh.configs.vite,
             eslintConfigPrettier,
-            importPlugin.flatConfigs.recommended,
+            importX.flatConfigs.recommended,
+            importX.flatConfigs.typescript,
         ],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
-        settings: {
-            'import/resolver': {
-                typescript: true,
-            },
-        },
         rules: {
-            'import/extensions': ['error', 'never'],
+            'import-x/extensions': ['error', 'never'],
         },
     },
     reactHooks.configs.flat['recommended-latest'],
