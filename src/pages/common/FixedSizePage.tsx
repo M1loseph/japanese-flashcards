3e79aedfa5d9c34e7cd4a92334638981ba652cd0
Miłosz Dyka <milosz.dyka@gmail.com>
2026-05-11 19:31:16 +0200

    better utilize limited space

--- src/pages/common/FixedSizePage.tsx
+++ src/pages/common/FixedSizePage.tsx
@@ -15,7 +15,7 @@ export const FixedSizePage: FC<FixedSizePageProps> = ({
     return (
         <div className="h-svh flex flex-col items-center font-sans">
             <Header preHomeNavigationHook={preHomeNavigationHook} additionalComponents={additionalHeaderComponents} />
-            <div className="container flex-1 min-h-0 overflow-hidden mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-12">
+            <div className="container flex-1 min-h-0 overflow-hidden mx-auto px-4 sm:px-6 lg:px-8 pt-8 space-y-12">
                 {children}
             </div>
         </div>
